c4f2307090e4b20a57e3b6069c4c0f3e3ee8c9b7
dan.dunai <dan.dunai@eu.rsystems.com>
2023-02-20 09:49:22 +0200

    initial commit

---
+++ src/components/Card.js
@@ -0,0 +1,12 @@
+import styles from './Card.module.css';
+
+function Card() {
+  return (
+    <div className={styles.card}>
+      <p>Card with background</p>
+    </div>
+    
+  );
+}
+
+export default Card;
